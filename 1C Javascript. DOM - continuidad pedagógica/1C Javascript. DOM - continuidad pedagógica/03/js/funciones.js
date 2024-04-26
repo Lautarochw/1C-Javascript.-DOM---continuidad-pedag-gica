2c241bdf13a12c081de99fdc11b2c363c1b6a524
Lautarochw <162642432+Lautarochw@users.noreply.github.com>
2024-04-26 19:46:42 -0300

    Add files via upload

---
+++ 1C Javascript. DOM - continuidad pedagógica/1C Javascript. DOM - continuidad pedagógica/03/js/funciones.js
@@ -0,0 +1,10 @@
+function cambiarColor() {
+    let tit = document.getElementById('titulo')
+    tit.style.color = '#ff0000'
+}
+
+function cambiarTamanoFuente() {
+    let tit = document.getElementById('titulo')
+    tit.style.fontSize = '60px'
+}
+
